29d2b00ea1e9301d92ea8b4d54890dce95d71430
eru123 <yeoligoakino@gmail.com>
2023-11-08 00:57:37 +0800

    entry: add error handler for global application, incase outside of route scope/context

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 require_once __DIR__ . '/autoload.php';
 
 use Filepark\Services\Domain;
+use eru123\router\Router;
 
-Domain::load_routes();
-Domain::run();
+try {
+    Domain::load_routes();
+    Domain::run();
+} catch (Throwable $e) {
+    Router::status_page(500, 'Internal Server Error', $e->getMessage());
+}
