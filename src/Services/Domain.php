7bf7b34c4a66cd950f1cf6d8a9cd53c675d8b9f6
eru123 <yeoligoakino@gmail.com>
2023-11-07 17:54:43 +0800

    domain: fix bugs and add improvements and new feats

--- src/Services/Domain.php
+++ src/Services/Domain.php
@@ -7,16 +7,19 @@ use eru123\router\Router;
 class Domain
 {
     static $domains = [];
+    static $usedCachedRoutes = false;
 
     /**
      * Create a new domain
      * @param string $domain The domain name (e.g. "filepark.skiddph.com")
      * @return Router
      */
-    public static function create($domain)
+    public static function create($domain, $autofix = true)
     {
-        if (is_null($domain)) {
-            $domain = isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] ? $_SERVER['SERVER_NAME'] : 'localhost';
+        if (is_null($domain) && $autofix) {
+            $domain = static::getDomain();
+        } else if (is_null($domain)) {
+            return new Router();
         }
 
         $domain = strtolower($domain);
@@ -43,8 +46,7 @@ class Domain
      */
     public static function run()
     {
-        $host = isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] ? $_SERVER['SERVER_NAME'] : 'localhost';
-        $host = strtolower($host);
+        $host = static::getDomain();
 
         if (!isset(self::$domains[$host])) {
             return Router::status_page(500, 'Internal Server Error', "Domain not found: " . htmlspecialchars($host));
@@ -74,6 +76,7 @@ class Domain
             }
         }
 
+        sort($result);
         return $result;
     }
 
@@ -83,9 +86,9 @@ class Domain
     public static function load_routes()
     {
         $map_file = __ROOT_DIR__ . '/routes_map.php';
-        if (file_exists($map_file)) {
+        if (!venv('CACHE_ROUTES', true) && venv('APP_ENV', 'production') == 'production' && file_exists($map_file)) {
+            self::$usedCachedRoutes = true;
             $routes = require($map_file);
-            echo "Loaded routes from cache\n";
         } else {
             $routes = self::routes_dirmap();
             if (is_writable(__ROOT_DIR__)) {
@@ -98,5 +101,22 @@ class Domain
                 require_once $route;
             }
         }
+    }
+
+    /**
+     * Check if cached routes are used
+     */
+    public static function isCachedRoutes()
+    {
+        return self::$usedCachedRoutes;
+    }
+
+    /**
+     * Get request's domain
+     */
+    public static function getDomain()
+    {
+        $domain = isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] ? $_SERVER['SERVER_NAME'] : 'localhost';
+        return strtolower($domain);
     }
 }
