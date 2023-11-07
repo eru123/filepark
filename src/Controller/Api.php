d666c2ef588b05c1be613c16e642dd35a298b950
eru123 <yeoligoakino@gmail.com>
2023-11-08 00:54:34 +0800

    api: init api controller

---
+++ src/Controller/Api.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Filepark\Controller;
+
+use Filepark\Services\Response;
+use eru123\router\Context;
+
+class Api {
+
+    public function upload(Context $ctx) {
+        
+    }
+
+}
