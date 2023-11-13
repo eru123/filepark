d3a3a7c19ed88d842f247545cee68bfdf3b5165d
eru123 <yeoligoakino@gmail.com>
2023-11-13 15:38:05 +0800

    response: added s3 file stream response

--- src/Services/Response.php
+++ src/Services/Response.php
@@ -22,4 +22,24 @@ class Response
     public static function apiDebug($router) {
         
     }
+
+    public static function s3_file_stream($data, $expires = '+1 hour') {
+        $size = $data['size'];
+        $type = $data['type'];
+
+        if (!headers_sent()) {
+            header('Content-Type: ' . $type);
+            header('Content-Length: ' . $size);
+            header('Content-Disposition: inline; filename="' . $data['name'] . '"');
+            header('Expires: ' . gmdate('D, d M Y H:i:s T', strtotime($expires)));
+            header('Cache-Control: max-age=' . (strtotime($expires) - time()));
+            header('Last-Modified: ' . gmdate('D, d M Y H:i:s T', strtotime($data['updated_at'])));
+            static::code(200);
+        }
+
+        foreach (S3::get_stream($data['path']) as $chunk) {
+            echo $chunk;
+        }
+        exit;
+    }
 }
